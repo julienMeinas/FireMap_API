76f0d72b00d22638c54e676ef0f89a6b6a2a8a70
julien <julien-9871@hotmail.fr>
2020-12-14 01:49:59 +0100

    add controller for fireworks

---
+++ src/main/java/com/istl/firemap_api/controller/FireworkController.java
@@ -0,0 +1,50 @@
+package com.istl.firemap_api.controller;
+
+import com.istl.firemap_api.bo.Firework;
+import com.istl.firemap_api.repository.FireworkRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+// tag::hateoas-imports[]
+// end::hateoas-imports[]
+
+@RestController
+public class FireworkController {
+
+	private final FireworkRepository repository;
+
+	public FireworkController(FireworkRepository repository) {
+		this.repository = repository;
+	}
+
+	// Aggregate root
+
+	// tag::get-aggregate-root[]
+	@GetMapping("/fireworks")
+	public List<Firework> all() {
+		return this.repository.findAll();
+	}
+	// end::get-aggregate-root[]
+
+	@PostMapping("/fireworks")
+	public Firework newEmployee(@RequestBody Firework firework) {
+		return repository.save(firework);
+	}
+
+	// Single item
+
+	// tag::get-single-item[]
+	@GetMapping("/fireworks/{id}")
+	public Optional<Firework> one(@PathVariable Long id) {
+		return this.repository.findById(id);
+	}
+	// end::get-single-item[]
+
+
+	@DeleteMapping("/fireworks/{id}")
+	public void deleteEmployee(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+}
