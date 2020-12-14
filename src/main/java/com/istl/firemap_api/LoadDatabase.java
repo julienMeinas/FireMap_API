21fbb18e3a6bc892a433264b6bf34e461a1880ed
julien <julien-9871@hotmail.fr>
2020-12-14 01:51:06 +0100

    add dataBase for initial data

---
+++ src/main/java/com/istl/firemap_api/LoadDatabase.java
@@ -0,0 +1,42 @@
+package com.istl.firemap_api;
+
+import com.istl.firemap_api.bo.Firework;
+import com.istl.firemap_api.bo.Fireworker;
+import com.istl.firemap_api.bo.Parking;
+import com.istl.firemap_api.repository.FireworkRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Configuration
+class LoadDatabase {
+
+	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+	@Bean
+	CommandLineRunner initDatabase(FireworkRepository repository) {
+		Fireworker fireworker = new Fireworker("Bob", 4.4);
+		List<Parking> parkings = new ArrayList<>();
+		parkings.add(new Parking("Parking du lac du héron", 0));
+
+		Fireworker fireworker1 = new Fireworker("Benjamin", 4.0);
+		List<Parking> parkings1 = new ArrayList<>();
+		parkings1.add(new Parking("Parking de la foire de Lille", 11));
+
+		Fireworker fireworker2 = new Fireworker("Julien", 4.5);
+		List<Parking> parkings2 = new ArrayList<>();
+		parkings2.add(new Parking("Parking de Antwerp centre", 20));
+
+		return args -> {
+			log.info("Preloading " + repository.save(new Firework(1.0005, 8.0009, "Lac du héron", new Date(), 0, true, 30, "High", fireworker, parkings)));
+			log.info("Preloading " + repository.save(new Firework(17.0007, 0.0008, "Foire de Lille", new Date(), 0, false, 20, "Low", fireworker1, parkings1)));
+			log.info("Preloading " + repository.save(new Firework(1.0007, 90.0008, "Antwerp centre", new Date(), 0, true, 45, "Medium", fireworker2, parkings2)));
+		};
+	}
+}
