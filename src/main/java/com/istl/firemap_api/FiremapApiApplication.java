d7dc5e837fff206f69e214d82caf62ccd4974b08
julien <julien-9871@hotmail.fr>
2020-12-14 01:42:20 +0100

    first commit

---
+++ src/main/java/com/istl/firemap_api/FiremapApiApplication.java
@@ -0,0 +1,13 @@
+package com.istl.firemap_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiremapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiremapApiApplication.class, args);
+    }
+
+}
