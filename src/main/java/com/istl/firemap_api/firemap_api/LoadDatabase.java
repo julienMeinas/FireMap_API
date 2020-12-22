e5cda391937f8f14adfd2eedadbe437e8246cfcf
julien <julien-9871@hotmail.fr>
2020-12-22 21:29:53 +0100

    update database

--- src/main/java/com/istl/firemap_api/firemap_api/LoadDatabase.java
+++ src/main/java/com/istl/firemap_api/firemap_api/LoadDatabase.java
@@ -34,19 +34,40 @@ class LoadDatabase {
 		List<Parking> parkings2 = new ArrayList<>();
 		parkings2.add(new Parking("Parking de Antwerp centre", 20));
 
+		Fireworker fireworker3 = new Fireworker("Nathalie", 0.5);
+		List<Parking> parkings3 = new ArrayList<>();
+		parkings3.add(new Parking("Parking de Tourcoing centre", 20));
+
+		Fireworker fireworker4 = new Fireworker("Elodie", 5);
+		List<Parking> parkings4 = new ArrayList<>();
+		parkings4.add(new Parking("Parking de Mons", 20));
+
+		Fireworker fireworker5 = new Fireworker("Nathalie", 0.5);
+		List<Parking> parkings5 = new ArrayList<>();
+		parkings5.add(new Parking("Parking de Marseille centre", 40));
+
+		Fireworker fireworker6 = new Fireworker("Elodie", 4.5);
+		List<Parking> parkings6 = new ArrayList<>();
+		parkings6.add(new Parking("Parking de Paris 7e", 50));
+
+		Fireworker fireworker7 = new Fireworker("François", 0.5);
+		List<Parking> parkings7 = new ArrayList<>();
+		parkings7.add(new Parking("Parking Roubaix", 40));
+
+		Fireworker fireworker8 = new Fireworker("Laurent", 4.5);
+		List<Parking> parkings8 = new ArrayList<>();
+		parkings8.add(new Parking("Parking sportpaleis Antwerp", 50));
+
 		return args -> {
 			log.info("Preloading " + repository.save(new Firework(1.0005, 8.0009, "Lac du héron", new Date(), 0, true, 30, "High", fireworker, parkings)));
 			log.info("Preloading " + repository.save(new Firework(17.0007, 0.0008, "Foire de Lille", new Date(), 0, false, 20, "Low", fireworker1, parkings1)));
 			log.info("Preloading " + repository.save(new Firework(1.0007, 90.0008, "Antwerp centre", new Date(), 5, true, 45, "Medium", fireworker2, parkings2)));
-			log.info("Preloading " + repository.save(new Firework(1.0005, 8.0009, "Lac du héron", new Date(), 0, true, 30, "High", fireworker, parkings)));
-			log.info("Preloading " + repository.save(new Firework(17.0007, 0.0008, "Foire de Lille", new Date(), 0, false, 20, "Low", fireworker1, parkings1)));
-			log.info("Preloading " + repository.save(new Firework(1.0007, 90.0008, "Antwerp centre", new Date(), 5, true, 45, "Medium", fireworker2, parkings2)));
-			log.info("Preloading " + repository.save(new Firework(1.0005, 8.0009, "Lac du héron", new Date(), 0, true, 30, "High", fireworker, parkings)));
-			log.info("Preloading " + repository.save(new Firework(17.0007, 0.0008, "Foire de Lille", new Date(), 0, false, 20, "Low", fireworker1, parkings1)));
-			log.info("Preloading " + repository.save(new Firework(1.0007, 90.0008, "Antwerp centre", new Date(), 5, true, 45, "Medium", fireworker2, parkings2)));
-			log.info("Preloading " + repository.save(new Firework(1.0005, 8.0009, "Lac du héron", new Date(), 0, true, 30, "High", fireworker, parkings)));
-			log.info("Preloading " + repository.save(new Firework(17.0007, 0.0008, "Foire de Lille", new Date(), 0, false, 20, "Low", fireworker1, parkings1)));
-			log.info("Preloading " + repository.save(new Firework(1.0007, 90.0008, "Antwerp centre", new Date(), 5, true, 45, "Medium", fireworker2, parkings2)));
+			log.info("Preloading " + repository.save(new Firework(170.0007, 40.0008, "Tourcoing centre", new Date(), 0, false, 10, "Low", fireworker3, parkings3)));
+			log.info("Preloading " + repository.save(new Firework(111.0007, 909.0008, "Mons", new Date(), 5, true, 15, "High", fireworker4, parkings4)));
+			log.info("Preloading " + repository.save(new Firework(10.0007, 40.008, "Marseille", new Date(), 0, false, 10, "High", fireworker5, parkings5)));
+			log.info("Preloading " + repository.save(new Firework(11.0007, 909.008, "Paris 7e", new Date(), 50, true, 60, "High", fireworker6, parkings6)));
+			log.info("Preloading " + repository.save(new Firework(10.0007, 40.008, "Roubaix", new Date(), 0, true, 10, "High", fireworker7, parkings7)));
+			log.info("Preloading " + repository.save(new Firework(11.0007, 909.008, "Antwerp sportpaleis", new Date(), 50, true, 60, "High", fireworker8, parkings8)));
 		};
 	}
 }
