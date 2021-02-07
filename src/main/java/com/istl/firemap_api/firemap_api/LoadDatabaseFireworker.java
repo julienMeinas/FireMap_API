568facb308d4b9e5f01f7ae382dcdd5bd6cfeda6
julien <julien-9871@hotmail.fr>
2021-02-07 22:48:24 +0100

    add fireworker & firework to database

--- src/main/java/com/istl/firemap_api/firemap_api/LoadDatabaseFireworker.java
+++ src/main/java/com/istl/firemap_api/firemap_api/LoadDatabaseFireworker.java
@@ -41,6 +41,9 @@ class LoadDatabaseFireworker {
 		avis5.add(new Avis(4.6, "C'était parfait !"));
 		List<String> photos5 = new ArrayList<>();
 
+
+		List<Avis> emptyAvis = new ArrayList<>();
+
 		return args -> {
 			log.info("Preloading " + controller.newFireworker(new FireworkerDetail(0, "Aucuns Artificiers", "", "", "", 0, new ArrayList<>(), new ArrayList<>(), "")));
 			log.info("Preloading " + controller.newFireworker(new FireworkerDetail(1, "Régie Fête Pyrotechnie", "regiefete.pyrotechnie@wanadoo.fr", "16 Chemin de la Grosse Borne, 62440 Harne", "03 21 20 32 96", 5, avis1, photos1, "https://www.facebook.com/RegieFete")));
@@ -48,6 +51,7 @@ class LoadDatabaseFireworker {
 			log.info("Preloading " + controller.newFireworker(new FireworkerDetail(3, "TOULOUSE ARTIFICES CREATIONS - TAC", "tac.capens@orange.fr", "ZAC de Serres - 15, rue des vieilles vignes - 31410 CAPENS", "05 61 90 34 15", 4.0, avis1, photos1, "http://feux-dartifice.com/")));
 			log.info("Preloading " + controller.newFireworker(new FireworkerDetail(4, "Feu d'artifice Hubert Thézé Pyrotechnie : spectacle pyrotechnique et illuminations de rue", "contact@htp.bzh", "Z.A. La Lande Rose 8 rue Blaise Pascal - 35580 GUICHEN", "02 99 52 06 35", 4.0, avis1, photos1, "https://www.htp.bzh/")));
 			log.info("Preloading " + controller.newFireworker(new FireworkerDetail(5, "Feu d'artifice Tourcoing, Wagnon artifices", "contact@wagnon-artifices.fr", "203 Rue de l’Alma 59100 Roubaix ", "03.20.70.74.75", 4.0, avis1, photos1, "https://www.wagnon-artifices.fr/")));
+			log.info("Preloading " + controller.newFireworker(new FireworkerDetail(6, "Bordeaux Grands Evènements 12 cours du XXX juillet BORDEAUX", "Pas de mail indiqué", "Pas d'adresse indiquée", "05 56 00 66 00", 4.0, emptyAvis, photos1, "Pad d'URL indiquée")));
 		};
 	}
 }
