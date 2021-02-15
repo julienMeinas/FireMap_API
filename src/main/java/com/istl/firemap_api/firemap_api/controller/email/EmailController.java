1036006fffda78bb7c1a6320d04541ed43b34619
martel <benjamin.martel.etu@univ-lille.fr>
2021-02-15 14:23:51 +0100

    Removed unused email service

--- src/main/java/com/istl/firemap_api/firemap_api/controller/email/EmailController.java
+++
@@ -1,7 +0,0 @@
-package com.istl.firemap_api.firemap_api.controller.email;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
-public interface EmailController {
-    public String sendEmail(String body, String subject);
-}
--- src/main/java/com/istl/firemap_api/firemap_api/service/email/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.istl.firemap_api.firemap_api.service.email;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface EmailService {
-    public String sendEmail(String body, String subject);
-}
